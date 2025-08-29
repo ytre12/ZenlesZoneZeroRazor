0a1390f6d4ea12caa91356ae420c8d64268cd01f
ytre12 <obio12yt@gmail.com>
2025-08-29 15:41:49 +0300

    Add files via upload

--- scipts/faqs.js
+++ scipts/faqs.js
@@ -15,11 +15,28 @@ const btnCloseBasket = document.getElementById("btn-close-basket");
 
 
 const buyCorinElement = document.getElementById("buy-corin");
+const buyRinalement = document.getElementById("buy-rina");
+const buyLicaonElement = document.getElementById("buy-licaon");
+const buyEllenElement = document.getElementById("buy-ellen");
 
 const btnClearBasket = document.getElementById("bay-items");
 
 const totalPriseBasket = document.getElementById("total-prise");
 const listBasketElement = document.getElementById("basket-items-list");
+const fullCartDetekted = document.getElementById("cart-full");
+const insinscriptionCartFull = document.getElementById("inscription-cart-full");
+
+
+const cartDetectedFunction = (itemsNow) => {
+    if (itemsNow >= 7) {
+        insinscriptionCartFull.hidden = false;
+        return false;
+    }else 
+    {
+        insinscriptionCartFull.hidden = true;
+        return true;
+    }
+}
 
 shopElement.hidden = true;
 exploreElement.hidden = true;
@@ -27,6 +44,8 @@ supportElement.hidden = true;
 companyElement.hidden = true;
 
 basketElement.hidden = true;
+
+insinscriptionCartFull.hidden = true;
 
 btnGetShop.onclick = function() {
     shopElement.hidden = !shopElement.hidden;
@@ -79,35 +98,145 @@ btnCloseBasket.onclick = function() {
 
 // btn buy items 
 
+let cartItemsNow = 0;
 let totalPrise = 0;
 const allPrise = 239.99;
+
 
 buyCorinElement.onclick = function() {
 
     basketElement.hidden = false;
 
-    const conteiner = document.createElement("div");
-    const name = document.createElement("p");
-    const prise = document.createElement("p");
+    if (cartDetectedFunction(cartItemsNow)) {
 
-    name.textContent = "Corin 1/7 Scale Figure";
-    prise.textContent = "239.99";
+        const conteiner = document.createElement("div");
+        const name = document.createElement("p");
+        const prise = document.createElement("p");
 
-    name.classList.add("text-seting-p");
-    prise.classList.add("prise-item-add");
+        name.textContent = "Corin 1/7 Scale Figure";
+        prise.textContent = "239.99";
 
-    conteiner.append(name, prise);
-    conteiner.classList.add("add-items-style");
+        name.classList.add("text-seting-p");
+        prise.classList.add("prise-item-add");
 
-    listBasketElement.insertAdjacentElement("afterbegin", conteiner);
+        conteiner.append(name, prise);
+        conteiner.classList.add("add-items-style");
 
-    totalPrise += allPrise;
-    totalPriseBasket.textContent = String(totalPrise) + " USD";
+        listBasketElement.insertAdjacentElement("afterbegin", conteiner);
+
+        totalPrise += allPrise;
+        cartItemsNow++;
+        totalPriseBasket.textContent = String(totalPrise) + " USD";
+        fullCartDetekted.textContent = "Your cart is full by (" + String(cartItemsNow) + " / 7)"; 
+    }
+}
+
+buyRinalement.onclick = function() {
+
+    basketElement.hidden = false;
+
+    if (cartDetectedFunction(cartItemsNow)) {
+
+        const conteiner = document.createElement("div");
+        const name = document.createElement("p");
+        const prise = document.createElement("p");
+
+        name.textContent = "Rina 1/7 Scale Figure";
+        prise.textContent = "239.99";
+
+        name.classList.add("text-seting-p");
+        prise.classList.add("prise-item-add");
+
+        conteiner.append(name, prise);
+        conteiner.classList.add("add-items-style");
+
+        listBasketElement.insertAdjacentElement("afterbegin", conteiner);
+
+        totalPrise += allPrise;
+        cartItemsNow++;
+        totalPriseBasket.textContent = String(totalPrise) + " USD";
+        fullCartDetekted.textContent = "Your cart is full by (" + String(cartItemsNow) + " / 7)"; 
+    }
+}
+
+buyLicaonElement.onclick = function() {
+
+    basketElement.hidden = false;
+
+    if (cartDetectedFunction(cartItemsNow)) {
+
+        const conteiner = document.createElement("div");
+        const name = document.createElement("p");
+        const prise = document.createElement("p");
+
+        name.textContent = "Von Lycaon 1/7 Scale Figure";
+        prise.textContent = "239.99";
+
+        name.classList.add("text-seting-p");
+        prise.classList.add("prise-item-add");
+
+        conteiner.append(name, prise);
+        conteiner.classList.add("add-items-style");
+
+        listBasketElement.insertAdjacentElement("afterbegin", conteiner);
+
+        totalPrise += allPrise;
+        cartItemsNow++;
+        totalPriseBasket.textContent = String(totalPrise) + " USD";
+        fullCartDetekted.textContent = "Your cart is full by (" + String(cartItemsNow) + " / 7)"; 
+    }
+}
+
+buyEllenElement.onclick = function() {
+
+    basketElement.hidden = false;
+
+    if (cartDetectedFunction(cartItemsNow)) {
+
+        const conteiner = document.createElement("div");
+        const name = document.createElement("p");
+        const prise = document.createElement("p");
+
+        name.textContent = "Ellen Joe  1/7 Scale Figure";
+        prise.textContent = "239.99";
+
+        name.classList.add("text-seting-p");
+        prise.classList.add("prise-item-add");
+
+        conteiner.append(name, prise);
+        conteiner.classList.add("add-items-style");
+
+        listBasketElement.insertAdjacentElement("afterbegin", conteiner);
+
+        totalPrise += allPrise;
+        cartItemsNow++;
+        totalPriseBasket.textContent = String(totalPrise) + " USD";
+        fullCartDetekted.textContent = "Your cart is full by (" + String(cartItemsNow) + " / 7)"; 
+    }
 }
 
 btnClearBasket.onclick = function() {
     listBasketElement.replaceChildren();
 
-    totalPrise = 0; 
+    totalPrise = 0;
+    cartItemsNow = 0; 
     totalPriseBasket.textContent = String(totalPrise) + " USD";
+    fullCartDetekted.textContent = "Your cart is full by (" + String(cartItemsNow) + " / 7)";
+
+    cartDetectedFunction(cartItemsNow);
+}
+
+// other ellements 
+
+const willBeSoon = document.getElementById("searth");
+const btnSerth = document.getElementById("searth-btn");
+
+willBeSoon.hidden = true;
+
+btnSerth.onclick = function() {
+    willBeSoon.hidden = false;
+}
+
+willBeSoon.onclick = function() {
+    willBeSoon.hidden = true;
 }
